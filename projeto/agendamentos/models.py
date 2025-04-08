df8f6a20c6da650c22382d9845da86ddcda36def
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-04-08 20:55:15 -0300

    feat: persistindo agendamento

--- projeto/agendamentos/models.py
+++ projeto/agendamentos/models.py
@@ -8,10 +8,32 @@ class Servico(models.Model):
         return self.nome
     
 class Agendamento(models.Model):
-    cliente = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
-    data_final = models.DateField()
-    date_created = models.DateTimeField(auto_now_add=True)
+    motivo = models.TextField(null=True)
+
+    # Usando caracteres como código para o status
+    STATUS_CHOICES = (
+        ('S', 'Solicitado'),
+        ('A', 'Andamento'),
+        ('F', 'Finalizado')
+    )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='S')
+
+    cliente = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE,
+        related_name='agendamentos_cliente'  # Nome exclusivo para a relação reversa de 'cliente'
+    )
+    bolsista = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='agendamentos_bolsista'  # Nome exclusivo para a relação reversa de 'bolsista'
+    )
+
+    data_prevista = models.DateField()
+    data_final = models.DateField(null=True)
+    data_de_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.cliente} - {self.servico} em {self.data_final.strftime('%d/%m/%Y')}"
+        return f"{self.cliente_id} - {self.servico}"
--- projeto/agendamentos/views.py
+++ projeto/agendamentos/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.shortcuts import get_object_or_404
-from .models import Agendamento
+from .models import Agendamento, Servico
 
 class AgendamentoView(View):
     def get(self, request):
-        return render(request, 'agendamentos/agendamento.html') 
+        servicos = Servico.objects.all()
+
+        contexto = {
+            'servicos': servicos,
+        }
+
+        return render(request, 'agendamentos/agendamento.html', contexto) 
+
+    def post(self, request):
+        servico_id = request.POST.get('servico')
+        servico = get_object_or_404(Servico, id=servico_id)
+
+        data_prevista = request.POST.get('data_prevista')
+        motivo = request.POST.get('motivo')
+
+        # Cria o agendamento com os dados do formulário
+        agendamento = Agendamento(
+            servico=servico,
+            cliente=request.user,
+            data_prevista=data_prevista,
+            motivo=motivo,
+        )
+
+        # Salvando o agendamento
+        agendamento.save()
+        
+        # Redirecionar ou renderizar uma resposta
+        return redirect('agendamento')  # Ajuste conforme necessário
